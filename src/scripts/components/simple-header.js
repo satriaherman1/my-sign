0e8c7c33dd67e9cf10737d85d5c85bd81fe07359
satriaherman <muhammadsatriaherman@gmail.com>
2024-02-18 14:57:25 +0700

    add example of importing components

---
+++ src/scripts/components/simple-header.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("simpleHeader").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/src/components/simple-header/index.html", true);
+  xhttp.send();
+};
