7b9bb700faa78e989cd59ca643b99a4edb87603a
satriaherman <muhammadsatriaherman@gmail.com>
2024-02-19 08:32:17 +0700

    add signature sidebar

--- src/scripts/components/signature-sidebar/preview.js
+++ src/scripts/components/signature-sidebar/preview.js
@@ -2,9 +2,10 @@ window.addEventListener("load", function () {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("simpleHeader").innerHTML = this.responseText;
+      document.getElementById("signature-sidebar").innerHTML =
+        this.responseText;
     }
   };
-  xhttp.open("GET", "/src/components/simple-header/index.html", true);
+  xhttp.open("GET", "/src/components/signature-sidebar/preview.html", true);
   xhttp.send();
 });
